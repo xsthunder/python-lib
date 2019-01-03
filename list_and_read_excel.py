7fc62b87f5beef1bc7168257dbe8b11aee39e502
xsthunder <xsthunder@outlook.com>
2019-01-03 22:16:32 +0800

    Update and rename list_file.py to list_and_read_excel.py

--- list_and_read_excel.py
+++ list_and_read_excel.py
@@ -4,3 +4,10 @@ from os.path import isfile, join
 arr = listdir('./')
 arr = list(filter(lambda x: x.find('xlsx')>=0,arr))
 name_arr = list(filter(lambda x:x.find('~$')<0, arr ))
+
+import pandas as pd
+from pandas_util import *
+p = print
+
+ef_arr = list(map(pd.ExcelFile, name_arr))
+df_arr = list(map(lambda x:x.parse(x.sheet_names[0],dtype=object), ef_arr))
