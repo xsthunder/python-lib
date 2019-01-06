b770a96f4668e733f8db18f0a2660895da778816
xsthunder <xsthunder@outlook.com>
2019-01-06 21:01:05 +0800

    Create analyse_columns.py

---
+++ analyse_columns.py
@@ -0,0 +1,13 @@
+def analyse_columns(df, columns=None, threshold=10):
+    """
+    filter len(df[column of columns].value_counts()) by threshold
+    will not filter if threshold < 0
+    reutrn arr: df[column].value_counts() of each column of columns in df
+    """
+    assert isinstance(df, pandas.DataFrame)
+    columns = df.columns if columns == None else columns
+    l = list(map(lambda x: df[x].value_counts(dropna=False), columns))
+    if(threshold < 0):
+        return l
+    else :
+        return list(filter(lambda x: len(x) < threshold, l))
