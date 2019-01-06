83aee2241e7f060e3b10238aaaefaa0295e34763
xsthunder <xsthunder@outlook.com>
2019-01-06 20:52:51 +0800

    Update rename_column.py

--- rename_column.py
+++ rename_column.py
@@ -4,7 +4,6 @@ def rename_column(df, name, new_name):
     assert isinstance(new_name, str)
     assert isinstance(df, pd.DataFrame)
     rename_dic = {}
-    rename_dic = {name:new_name}
-    print(rename_dic)
+    rename_dic = {name:new_name}# notes difference with {name, new_name}
     df = df.rename(index=str, columns=rename_dic)
     return df
