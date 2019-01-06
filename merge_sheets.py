bdfd6aa7f564ca43af209ff660c84fe9bd10ae35
xsthunder <xsthunder@outlook.com>
2019-01-06 20:56:33 +0800

    Update and rename merge_sub_sheets.py to merge_sheets.py

--- merge_sheets.py
+++ merge_sheets.py
@@ -1,6 +1,6 @@
 from functools import reduce
 import pandas
-def merge_sub_sheets(excel_file):
+def merge_sheets(excel_file):
     assert isinstance(excel_file, pandas.io.excel.ExcelFile) 
     sheet_names = excel_file.sheet_names
     dfs = list(map(excel_file.parse, sheet_names))
