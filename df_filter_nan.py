f612faa7bb1a2749f971a51f23cd142e8ba510e6
xsthunder <xsthunder@outlook.com>
2019-01-06 19:41:48 +0800

    Create df_filter_nan.py

---
+++ df_filter_nan.py
@@ -0,0 +1,8 @@
+import numpy
+import pandas
+def filter_nan(df):
+  """
+  return dataframe consisting rows which don't contain nan value
+  """
+  assert isinstance(df, pandas.DataFrame), "wrong type:%s, expected %s"%(type(df), pandas.DataFrame)
+  return df[df.apply(lambda sr: all(map(lambda x:~numpy.isnan(x),  list(sr))), axis=1)]
