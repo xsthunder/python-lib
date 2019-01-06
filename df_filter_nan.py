0e14f7411d48dcae787e6c0fbb664a6a945d8aa2
xsthunder <xsthunder@outlook.com>
2019-01-06 23:05:57 +0800

    Update df_filter_nan.py

--- df_filter_nan.py
+++ df_filter_nan.py
@@ -1,9 +1,9 @@
 import numpy
 import pandas
 def filter_nan(df):
-  """
-  return dataframe consisting rows which don't contain nan value
-  """
-  assert isinstance(df, pandas.DataFrame), "wrong type:%s, expected %s"%(type(df), pandas.DataFrame)
-  return df[df.apply(lambda sr: all(map(lambda x:~numpy.isnan(x),  list(sr))), axis=1)]
+    """
+    return dataframe consisting rows which don't contain nan value
+    """
+    assert isinstance(df, pandas.DataFrame), "wrong type:%s, expected %s"%(type(df), pandas.DataFrame)
+    return df[df.apply(lambda sr: all(map(lambda x:~numpy.isnan(x),  list(sr))), axis=1)]
 
