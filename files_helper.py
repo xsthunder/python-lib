d31dd5e0c1f779897f77bdf4b23e27729c47a0f5
xsthunder <xsthunder@outlook.com>
2019-01-09 18:24:20 +0800

    Update and rename list_and_read_excel.py to files_helper.py

---
+++ files_helper.py
@@ -0,0 +1,17 @@
+from os import listdir
+from os.path import isfile, join
+import pandas as pd
+
+def read_excel_file_name(dir = './', contains=['.xlsx'], notcontains=[''~$'']):
+    arr = listdir(dir)
+    for s in contains:
+        arr = list(filter(lambda x: x.find(s)>=0,arr))
+    for s in notcontains:
+        arr = list(filter(lambda x:x.find('~$')<0, arr ))
+    name_arr = arr
+    path_arr = list(map(lambda x:dir+x, name_arr))
+    return path_arr,name_arr
+
+def read_ef_arr(path_arr):
+    ef_arr = list(map(pd.ExcelFile, path_arr))
+
