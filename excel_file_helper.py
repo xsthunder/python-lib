36eeb7a178fe32d5e5746a72bba2281761a1549c
xsthunder <xsthunder@outlook.com>
2019-01-09 18:24:22 +0800

    Update and rename merge_sheets.py to excel_file_helper.py

--- excel_file_helper.py
+++ excel_file_helper.py
@@ -1,7 +1,11 @@
 from functools import reduce
 import pandas
+
 def merge_sheets(excel_file):
     assert isinstance(excel_file, pandas.io.excel.ExcelFile) 
     sheet_names = excel_file.sheet_names
     dfs = list(map(excel_file.parse, sheet_names))
     return reduce(lambda x,y: x.append(y), dfs)
+
+def read_df_arr(ef_arr):
+    df_arr = list(map(lambda x:x.parse(x.sheet_names[0],dtype=object), ef_arr))
