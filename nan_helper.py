a922a6a1c8af9dfcbf99bcd91ac3e119a8232021
xsthunder <xsthunder@outlook.com>
2019-01-10 11:27:25 +0800

    Update nan_helper.py

--- nan_helper.py
+++ nan_helper.py
@@ -1,20 +1,30 @@
 import numpy
 import pandas
-def filter_nan(df):
+def is_nan(x, empty_str_as_nan = True, on_unknow_type = True):
+    if( isinstance(x, str) ):
+        if(empty_str_as_nan):
+            return x == ''
+        else :
+            return False
+    if( isinstanc(x, numpy.float) ):
+        return numpy.isnan(x)
+    return on_unknow_type
+
+def filter_nan(df,empty_str_as_nan = True, on_unknow_type = True):
     """
     return series indicating dataframe consisting rows which don't contain nan value
     you may use df[ret]
     support array bool op such as &
     """
     assert isinstance(df, pandas.DataFrame), "wrong type:%s, expected %s"%(type(df), pandas.DataFrame)
-    return df.apply(lambda sr: all(map(lambda x:~numpy.isnan(x),  list(sr))), axis=1)
+    return df.apply(lambda sr: all(map(lambda x:~is_nan(x,empty_str_as_nan,on_unknow_type ),  list(sr))), axis=1)
 
-def filter_has_nan(df):
+def filter_has_nan(df,empty_str_as_nan = True, on_unknow_type = True):
     """
     return series indicating dataframe consisting rows which contain any nan value
     you may use df[ret]
     support array bool op such as &
     """
     assert isinstance(df, pandas.DataFrame), "wrong type:%s, expected %s"%(type(df), pandas.DataFrame)
-    return df.apply(lambda sr: any(map(lambda x:numpy.isnan(x),  list(sr))), axis=1)
+    return df.apply(lambda sr: any(map(lambda x:is_nan(x,empty_str_as_nan,on_unknow_type ),  list(sr))), axis=1)
 
