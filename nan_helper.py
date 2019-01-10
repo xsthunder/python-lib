4f6cf0d6dedd6403024a43e1c3501ffb8284a103
xsthunder <xsthunder@outlook.com>
2019-01-10 11:16:37 +0800

    Update and rename df_filter_nan.py to nan_helper.py

--- nan_helper.py
+++ nan_helper.py
@@ -9,3 +9,12 @@ def filter_nan(df):
     assert isinstance(df, pandas.DataFrame), "wrong type:%s, expected %s"%(type(df), pandas.DataFrame)
     return df.apply(lambda sr: all(map(lambda x:~numpy.isnan(x),  list(sr))), axis=1)
 
+def filter_has_nan(df):
+    """
+    return series indicating dataframe consisting rows which don't contain nan value
+    you may use df[ret]
+    support array bool op such as &
+    """
+    assert isinstance(df, pandas.DataFrame), "wrong type:%s, expected %s"%(type(df), pandas.DataFrame)
+    return df.apply(lambda sr: any(map(lambda x:numpy.isnan(x),  list(sr))), axis=1)
+
