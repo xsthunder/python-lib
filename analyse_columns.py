8573746292731158a71a14447897a44f35b762cb
xsthunder <xsthunder@outlook.com>
2019-03-01 10:27:17 +0800

    Update analyse_columns.py

--- analyse_columns.py
+++ analyse_columns.py
@@ -1,5 +1,5 @@
 import pandas
-def analyse_columns(df, columns=None, threshold=-1):
+def analyse_columns(df, columns=None, threshold=-1, to_value_count=False):
     """
     filter len(df[column of columns].value_counts()) by threshold
     will not filter if threshold < 0
@@ -8,9 +8,11 @@ def analyse_columns(df, columns=None, threshold=-1):
     assert isinstance(df, pandas.DataFrame)
     columns = df.columns if columns == None else columns
     l = list(map(lambda x: df[x].value_counts(dropna=False), columns))
+    ret = list(filter(lambda x: len(x) < threshold, l))
     if(threshold < 0):
-        return l
-    else :
-        return list(filter(lambda x: len(x) < threshold, l))
+        ret = l
+    if(to_value_count):
+        return list(map(lambda sr:(sr.name, len(sr)), ret))
+    return ret
 
     
