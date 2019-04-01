c7157eede255ef5879a37043a406dc8c472cc8d0
xsthunder <xsthunder@outlook.com>
2019-04-01 09:11:54 +0800

    Update json_helper.py

--- json_helper.py
+++ json_helper.py
@@ -2,3 +2,5 @@ import json
 def save_as_json(obj,file_name, ensure_ascii=False):
     f = open(file_name,'w')
     json.dump( obj, fp=f,ensure_ascii=ensure_ascii)
+def sample_dict(dic, count=5):
+    return list(dic.items())[:count]
