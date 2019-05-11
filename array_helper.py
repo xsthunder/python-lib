0b15248e6fd867be6aa055c8416a367ba086d4fb
xsthunder <xsthunder@outlook.com>
2019-05-11 10:57:24 +0800

    Update array_helper.py

--- array_helper.py
+++ array_helper.py
@@ -1,4 +1,8 @@
 def flatten_array(array):
+    """
+    @params array, dim >=2
+    @return new array, dim = dim - 1
+    """
     def f(ac, arr):
         if(isinstance(arr, list)):
             ac.extend(arr)
