b6b5e9cbcd1ee50330c4f44260ad29ddd32a08f6
xsthunder <xsthunder@outlook.com>
2019-05-13 09:10:56 +0800

    Update json_helper.py

--- json_helper.py
+++ json_helper.py
@@ -4,3 +4,5 @@ def save_as_json(obj,file_name, ensure_ascii=False):
     json.dump( obj, fp=f,ensure_ascii=ensure_ascii)
 def sample_dict(dic, count=5):
     return list(dic.items())[:count]
+def load_as_json(file_name):
+    jy = json.load(open(file_name))
